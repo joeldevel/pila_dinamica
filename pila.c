a1648d94a817e92e58545eb235a64c44f5f0382a
joeldevel <christianjoel.developer@gmail.com>
2020-10-31 20:53:50 -0300

    final version

--- pila.c
+++ pila.c
@@ -1,9 +1,9 @@
 #include "pila.h"
-#include<stdio.h>
 #include <stdlib.h>
 
 #define MIN_PILA 4 // el minimo que la pila al ser creada
 #define FACTOR_INC_PILA 2 // duplicar la pila cuando llena
+#define FACTOR_DEC_PILA 2 // dividir la pila cuando pocos elementos
 
 /* Definición del struct pila proporcionado por la cátedra.
  */
@@ -12,153 +12,66 @@ struct pila {
     size_t cantidad;   // Cantidad de elementos almacenados.
     size_t capacidad;  // Capacidad del arreglo 'datos'.
 };
-
+// función auxiliar
+void *pila_redimensionar(pila_t *pila, size_t nueva_capacidad) {
+    void *datos_redimensionados = realloc(pila->datos, nueva_capacidad * sizeof(void*));
+    if (!datos_redimensionados) return NULL;
+    pila->capacidad = nueva_capacidad;
+    pila->datos = datos_redimensionados;
+    return pila;
+}
 /* *****************************************************************
  *                    PRIMITIVAS DE LA PILA
  * *****************************************************************/
 
 // ..
 pila_t *pila_crear(void) {
-    // crear un puntero struct en heap
-    pila_t *pila = malloc( sizeof(pila_t) );
-    // si no lo pudo crear retornar null
+    pila_t *pila = malloc(sizeof(pila_t));
     if (!pila) return NULL;
-    // si todo ok sigo
-    // crear un puntero generico en heap de size puntero generico
-    // * minimo size de la pila
-    void *datos = malloc( sizeof(void*) * MIN_PILA);
-    // si no se puede retornar null
-    if( !datos) {
-      printf("no se pudo hacer malloc datos\n");
-      return NULL;
-    }
+    void *datos = malloc(sizeof(void*) * MIN_PILA);
     pila->datos = datos;
     pila->cantidad = 0;
     pila->capacidad = MIN_PILA;
-    // printf("pila->datos: %p\n", pila->datos);
-    // printf("pila->capacidad: %d\n", pila->capacidad);
-    // printf("pila->cantidad: %d\n", pila->cantidad);
-
-    // todo ok, retorno la pila, que vive el el heap
+    if (!datos) {
+        free(pila);
+        return NULL;
+    }
     return pila;
 }
 
 void pila_destruir(pila_t *pila) {
-    // borro los datos de la pila primero
-  free(pila->datos);
-  // luego la pila
-  free(pila);
+    free(pila->datos);
+    free(pila);
 }
 
 bool pila_esta_vacia(const pila_t *pila) {
-
     return pila->cantidad == 0;
 }
 
 bool pila_apilar(pila_t *pila, void *valor) {
-    // solamente contemplado el caso de tudo bom
-    // printf("apilando %d\n",  (int)valor);
-    // pongo el el arreglo de datos el valor en la posicion
-    //  cantidad (inicialmente = 0) e incremento la cantidad
-    if( pila->cantidad == pila->capacidad) {
-      // intentar el realloc
-      void *datos_nuevo = realloc(pila->datos, pila->cantidad *FACTOR_INC_PILA * sizeof(void*));
-
-      // Cuando tam_nuevo es 0, es correcto si se devuelve NULL.
-      // En toda otra situación significa que falló el realloc.
-      if ( datos_nuevo == NULL) {
-          return false;
-      }
-      // poner los datos en la pila
-      pila->datos = datos_nuevo;
-      // actualizar cantidad
-      pila->capacidad = pila->cantidad* FACTOR_INC_PILA;
+    if (pila->cantidad == pila->capacidad-1) {
+        void *pila_redimensionada = pila_redimensionar(pila,pila->capacidad*FACTOR_INC_PILA);
+        if (pila_redimensionada == NULL) {
+             return false;
+         }
     }
-    // agregar el dato
     pila->datos[pila->cantidad] = valor;
     pila->cantidad++;
-    // printf("pila en cantidad %d\n",  (int)pila->datos[0]);
-    printf("pila->capacidad: %d\n", pila->capacidad);
-    printf("pila->cantidad: %d\n", pila->cantidad);
-
-  return true;
+    return true;
 }
 
 void *pila_ver_tope(const pila_t *pila) {
-    // pila vacia
-    if(pila_esta_vacia(pila) ) return NULL;
-    // lo jodido de esta funcion es el return
-    // paso a puntero generico la direcion del dato[tope]
-    // ojo al piojo!
-    // printf("cantidad es %d\n", (int)pila->cantidad);
-    // printf("tope de pila: %d\n", (int)pila->datos[pila->cantidad-1]);
-  return (void *)&pila->datos[pila->cantidad-1];
+    if (pila_esta_vacia(pila)) return NULL;
+    return pila->datos[pila->cantidad-1];
 }
 
 void *pila_desapilar(pila_t *pila) {
-    if(pila->cantidad == 0) return NULL;
-    /*
-    if(pila->cantidad == 1) {
-        pila->cantidad = 0;
-        return (void *)&pila->datos[0];
+    if (pila->cantidad == 0) return NULL;
+    if ((pila->capacidad > MIN_PILA*4) && (pila->cantidad <= pila->capacidad/4)) {
+      void *pila_redimensionada = pila_redimensionar(pila,pila->capacidad/FACTOR_DEC_PILA);
+      if (pila_redimensionada == NULL) {
+           return false;
+       }
     }
-    pila->cantidad--;
-    */
-    if( (pila->capacidad > MIN_PILA*4) && (pila->cantidad <= pila->capacidad/4)) {
-      // realloc para abajo
-      void *datos_nuevo = realloc(pila->datos, (pila->capacidad / 2) * sizeof(void*));
-
-      // Cuando tam_nuevo es 0, es correcto si se devuelve NULL.
-      // En toda otra situación significa que falló el realloc.
-      // if ( datos_nuevo == NULL) {
-      //     return false;
-      // }
-      // poner los datos en la pila
-      pila->datos = datos_nuevo;
-      // actualizar cantidad
-      pila->capacidad = pila->capacidad/2;
-    }
-      // actualizar capacidad
-    // }
-    // Aguantaaa Maradona!
-    return (void *)&pila->datos[--pila->cantidad];
+    return pila->datos[--(pila->cantidad)];
 }
-/*
-int main() {
-    pila_t *pi = pila_crear();
-    //free(pi->datos);
-    //free(pi);
-    // pruebo de poner un elemento en la pila, debo pasarle un puntero
-   // void* v = malloc( sizeof(void *)*1 );
-   // v = (int*)8897;
-   // printf("v=%d , &v= %p\n", (int)v, &v);
-    pila_apilar(pi,(int*)3323);
-    pila_apilar(pi, (int*)2172);
-
-    // void *  eltope = malloc(sizeof(void*));
-    // ojo con esto, pila_ver_tope me devuelve una direccion de
-    // un puntero generico, (int *) lo transforma en un puntero a entero
-    // y * antes del (int *) desreferencia la direccion del tope
-    // y lo guardo en eltope
-    int eltope = *(int *)pila_ver_tope(pi);
-    // muestro el tope como un entero comun y corriente
-    printf("al tope esta: %d\n",eltope);
-    if( pila_esta_vacia( pi ) ) printf("La pila esta vacia\n");
-    else printf("La pIla NO esta vacia\n");
-
-    printf("------------------desapilar--------\n");
-    int saco_elemento = *(int *)pila_desapilar(pi);
-    printf("elemento sacado: %d\n",saco_elemento);
-
-    if( pila_esta_vacia( pi ) ) printf("La pila esta vacia\n");
-    else printf("La pIla NO esta vacia\n");
-
-    eltope = *(int *)pila_ver_tope(pi);
-    // muestro el tope como un entero comun y corriente
-    printf("al tope esta: %d\n",eltope);
-
-
-    pila_destruir( pi );
-  return 0;
-}
-*/
--- pruebas_pila.c
+++ pruebas_pila.c
@@ -2,102 +2,172 @@
 #include "testing.h"
 #include<stdio.h>
 
+/* Pruebas para una pila vacía. */
 static void prueba_pila_vacia(void) {
+
+    printf("INICIO DE PRUEBAS CREAR PILA VACIA\n");
+
     pila_t *pila = pila_crear();
-    // print_test( ... pila_esta_vacia(pila) ... );
     print_test("Prueba pila vacía: ", pila_esta_vacia(pila));
     pila_destruir(pila);
 }
-// lo mismo que la de arriba?
+
+/* Pruebas para tope pila vacía es inválido. */
 static void prueba_pila_ver_tope_creada_es_invalido(void) {
+
+    printf("INICIO DE PRUEBAS VER TOPE EN PILA VACÍA ES INVÁLIDO\n");
+
     pila_t *pila = pila_crear();
-    print_test("Prueba pila ver tope en pila recien creada es invalido: ", pila_ver_tope(pila)==NULL);
+    print_test("Prueba pila ver tope en pila recién creada es inválido: ", pila_ver_tope(pila)==NULL);
     pila_destruir(pila);
 }
+
+/* Pruebas para apilar un elemento. */
+static void prueba_apilar_un_elemento(void) {
+
+    printf("INICIO DE PRUEBAS APILAR UN ELEMENTO\n");
+
+    pila_t *pila = pila_crear();
+  
+    char elemento = 'A';
+
+    bool ok = pila_apilar(pila, &elemento);
+    print_test("Se puede apilar un elemento", ok);
+
+    int tope = *(char*)pila_ver_tope(pila);
+    printf("lo que vuelve de pila_ver_tope= %c\n", tope );
+
+    pila_destruir(pila);
+}
+
+/* Pruebas para apilar pocos elementos. */
 static void prueba_apilar_pocos_elementos(void) {
-    pila_t *pila = pila_crear();
-    for(int i=0; i< 10; i++) {
-      pila_apilar(pila, (char *)'j');
-      print_test("tope de pila = j: ", ('j'==*(char *)pila_ver_tope(pila)));
-      pila_apilar(pila, (int *)219);
-      print_test("tope de pila = 219: ", (219==*(int*)pila_ver_tope(pila)));
 
+    printf("INICIO DE PRUEBAS APILAR POCOS ELEMENTOS\n");
+
+    pila_t *pila = pila_crear();
+    char elementos[] = {'A','b','c', 'F','X'};
+    int tam = 5;
+    bool ok = true;
+    for(int i=0; i < tam; i++) {
+        pila_apilar(pila, &elementos[i]);
+        ok &= (elementos[i]==*(char*)pila_ver_tope(pila));
     }
+
+    print_test("se pudieron apilar todos los elementos", ok);
+
     pila_destruir(pila);
 }
+
+/*Se pueden desapilar los elementos y se cumple FIFO*/
 static void prueba_pila_desapilar(void) {
-  pila_t *pila = pila_crear();
-  int valores[] = {11,22,33,44,55,66,77,88,99};
-  int cant_valores = 9;
-  for(int i = 0; i< cant_valores; i++) {
-    pila_apilar(pila, (int*)valores[i]);
-  }
-  for( int i = 0; i< cant_valores;i++) {
-    print_test("desapilando: ",(valores[cant_valores-i-1]==*(int*)pila_desapilar(pila)));
-  }
-  pila_destruir(pila);
+
+    printf("INICIO DE PRUEBAS DESAPILAR\n");
+
+    pila_t *pila = pila_crear();
+    int valores[] = {11,22,33,44,55,66,77,88,99};
+    int tam = 9;
+    for (int i = 0; i < tam; i++) {
+        pila_apilar(pila, &valores[i]);
+    }
+
+    bool ok = true;
+    for (int i = 0; i < tam; i++) {
+      ok &= (valores[tam-i-1]==*(int*)pila_desapilar(pila));
+    }
+    print_test("se pudieron desapilar todos los elementos", ok);
+
+    pila_destruir(pila);
 }
+
+/* Pruebas de la pila al trabajar con un volumen grande de elementos */
 static void prueba_pila_volumen(void) {
-  pila_t *pila = pila_crear();
-  /* Pruebo guardar en todas las posiciones */
-  int tam = 10;
-  int i;
-  bool ok = true;
-  for (i = 0; i < tam; i++) {
-      // Si algun elemento no se pudo guardar correctamente, ok sera false
-      ok &= pila_apilar(pila, (int*)i);
-  }
-  print_test("se pudieron apilar todos los elementos", ok);
 
-  // desapilarlos
-  for( int i = 0; i< tam;i++) {
-    printf("%d\n", i);
-    print_test("desapilando: ",(tam-i-1==*(int*)pila_desapilar(pila)));
-  }
-  // print_test("se pudieron desapilar todos los elementos", ok);
+    printf("INICIO DE PRUEBAS VOLUMEN\n");
 
-  // print_test("no se puede apilarun elemento en vec_vol[tam]", !vector_guardar(vec_vol, tam, 10));
+    pila_t *pila = pila_crear();
 
+    int tam = 1000;
+    int el[tam];
+    for (int i = 0; i< tam; i++) {
+      el[i] = i;
+    }
+    bool ok = true;
+    for (int i = 0; i < tam; i++) {
+        // Si algun elemento no se pudo guardar correctamente, ok sera false
+        ok &= pila_apilar(pila, &el[i]);
+    }
+    print_test("se pudieron apilar todos los elementos", ok);
 
-  pila_destruir(pila);
+    // desapilarlos
+    ok = true;
+    for (int i = 0; i < tam; i++) {
+        ok &= (tam-i-1==*(int*)pila_desapilar(pila));
+    }
+    print_test("se pudieron desapilar todos los elementos", ok);
+
+    pila_destruir(pila);
 }
 static void prueba_pila_desapilar_vacia_es_invalido(void) {
-  pila_t *pila = pila_crear();
-  print_test("Prueba pila vacía: ", pila_esta_vacia(pila));
-  print_test("Prueba desapilar vacia es invalido", pila_desapilar(pila)==NULL);
-  pila_destruir(pila);
+
+    printf("INICIO DE PRUEBAS DESAPILAR PILA VACÍA ES INVÁLIDO\n");
+
+    pila_t *pila = pila_crear();
+    print_test("Prueba pila vacía: ", pila_esta_vacia(pila));
+    print_test("Prueba desapilar vacía es inválido", pila_desapilar(pila)==NULL);
+    pila_destruir(pila);
 }
 
-static void prueba_pila_desapilar_despues_de_apilar_y_desapilar_es_invalido(  void){
-  pila_t *pila = pila_crear();
-  int valores[] = {11,22,33,44,55,66,77,88,99};
-  int cant_valores = 9;
-  for(int i = 0; i< cant_valores; i++) {
-    pila_apilar(pila, (int*)valores[i]);
-  }
-  for( int i = 0; i< cant_valores;i++) {
-    print_test("desapilando: ",(valores[cant_valores-i-1]==*(int*)pila_desapilar(pila)));
-  }
-  print_test("Prueba desapilar despues de apilar y vaciar es invalido", pila_desapilar(pila)==NULL);
+static void prueba_pila_desapilar_despues_de_apilar_y_desapilar_es_invalido(void) {
 
-  pila_destruir(pila);
-}
-static void prueba_pila_ver_tope_despues_de_apilar_y_desapilar_es_invalido(  void){
-  pila_t *pila = pila_crear();
-  int valores[] = {11,22,33,44,55,66,77,88,99};
-  int cant_valores = 9;
-  for(int i = 0; i< cant_valores; i++) {
-    pila_apilar(pila, (int*)valores[i]);
-  }
-  for( int i = 0; i< cant_valores;i++) {
-    print_test("desapilando: ",(valores[cant_valores-i-1]==*(int*)pila_desapilar(pila)));
-  }
-  print_test("Prueba ver tope despues de apilar y vaciar es invalido", pila_desapilar(pila)==NULL);
+    printf("INICIO DE PRUEBAS DESAPILAR DESPUES DE DESAPILAR INVÁLIDO\n");
 
-  pila_destruir(pila);
+    pila_t *pila = pila_crear();
+    int valores[] = {11,22,33,44,55,66,77,88,99};
+    int cant_valores = 9;
+    for(int i = 0; i< cant_valores; i++) {
+        pila_apilar(pila, &valores[i]);
+    }
+    for( int i = 0; i< cant_valores;i++) {
+        pila_desapilar(pila);
+    }
+    print_test("Prueba desapilar despues de apilar y vaciar es inválido", pila_desapilar(pila)==NULL);
+
+    pila_destruir(pila);
 }
+
+
+static void prueba_pila_ver_tope_despues_de_apilar_y_desapilar_es_invalido(void) {
+
+    printf("INICIO DE PRUEBAS VER TOPE DESPUES DE DESAPILAR INVÁLIDO\n");
+
+    pila_t *pila = pila_crear();
+    int valores[] = {11,22,33,44,55,66,77,88,99};
+    int cant_valores = 9;
+    for(int i = 0; i< cant_valores; i++) {
+      pila_apilar(pila, &valores[i]);
+    }
+    for( int i = 0; i< cant_valores;i++) {
+        pila_desapilar(pila);
+    }
+    print_test("Prueba ver tope despues de apilar y vaciar es invalido", pila_desapilar(pila)==NULL);
+
+    pila_destruir(pila);
+}
+
+/* Se puede apilar NULL */
+static void prueba_pila_apilar_null() {
+
+    printf("INICIO DE PRUEBAS APILAR NULL ES VÁLIDO\n");
+
+    pila_t *pila = pila_crear();
+    print_test("Prueba se puede apilar NULL", pila_apilar(pila, NULL));
+    pila_destruir(pila);
+}
+
 void pruebas_pila_estudiante() {
     prueba_pila_vacia();
+    prueba_apilar_un_elemento();
     prueba_pila_ver_tope_creada_es_invalido();
     prueba_apilar_pocos_elementos();
     prueba_pila_desapilar();
@@ -105,7 +175,7 @@ void pruebas_pila_estudiante() {
     prueba_pila_desapilar_vacia_es_invalido();
     prueba_pila_desapilar_despues_de_apilar_y_desapilar_es_invalido();
     prueba_pila_ver_tope_despues_de_apilar_y_desapilar_es_invalido();
-    // ...
+    prueba_pila_apilar_null();
 }
 
 
